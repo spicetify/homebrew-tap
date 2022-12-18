68765382393bf2906b33a29512a7580ccf4d44a4
Isaac Maier <padspambox@gmail.com>
2022-12-18 16:41:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# spicetify-cli homebrew tap
+
+*[spicetify-cli](https://github.com/spicetify/spicetify-cli)'s homebrew tap*
+
+## Installation
+
+```sh 
+brew install spicetify/tap/spicetify-cli
+```
+
+for more detailed guide see [wiki](https://github.com/spicetify/spicetify-cli/wiki/Installation)
+
+## :warning: :warning: **The brew version may be outdated** :warning: :warning:
+
+After installing don't forget to update it to the latest version using:
+
+```sh
+spicetify upgrade
+```
+
+Make sure you run this after upgrading:
+
+```sh
+spicetify restore backup apply
+```
---
+++ spicetify-cli.rb
@@ -0,0 +1,30 @@
+require "language/go"
+
+class SpicetifyCli < Formula
+  desc "Command-line tool to customize Spotify client"
+  homepage "https://github.com/spicetify/spicetify-cli"
+  url "https://github.com/spicetify/spicetify-cli/archive/v2.14.3.tar.gz"
+  head "https://github.com/spicetify/spicetify-cli"
+  sha256 "a2d4c36070193f6838ac48cccf920f0a3c793876294bc3f1ed64c6a603a384db"
+
+  depends_on "go" => "1.18"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    path = buildpath/"dep"
+    path.install Dir["*"]
+    cd path do
+      system "go", "build", "-ldflags", "-X main.version=#{version.to_s}", "-o", "#{bin}/spicetify"
+      cp_r "./globals.d.ts", bin
+      cp_r "./jsHelper", bin
+      cp_r "./Themes", bin
+      cp_r "./Extensions", bin
+      cp_r "./CustomApps", bin
+      cp_r "./css-map.json", bin
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spicetify", "-v")
+  end
+end
