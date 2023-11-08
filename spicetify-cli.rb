a21438b44eaddd986d84cd600bbb02752ca1f43f
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-11-08 17:24:32 +0000

    feat: 2.27.0 release

--- spicetify-cli.rb
+++ spicetify-cli.rb
@@ -3,9 +3,9 @@ require "language/go"
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://github.com/spicetify/spicetify-cli"
-  url "https://github.com/spicetify/spicetify-cli/archive/v2.26.0.tar.gz"
+  url "https://github.com/spicetify/spicetify-cli/archive/v2.27.0.tar.gz"
   head "https://github.com/spicetify/spicetify-cli"
-  sha256 "7a2e6106a0be70b75ede8b31fae647be4c0a57eb419c93fa9d42963ab1039d32"
+  sha256 "824ac8341f5833277a7177a7e17af7036a173e01acbc0730df619c035e0de16b"
 
   depends_on "go" => "1.19"
 
